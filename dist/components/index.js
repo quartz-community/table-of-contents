3dbab5894b20d725b16601759ed9d552ba4f553e
saberzero1 <github@emilebangma.com>
2026-07-23 00:40:22 +0200

    chore: migrate to npm publishing

--- dist/components/index.js
+++ dist/components/index.js
@@ -1,4 +1,4 @@
-// node_modules/@quartz-community/utils/dist/lang.js
+// ../utils/dist/lang.js
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
@@ -328,7 +328,7 @@ l = { __e: function(n2, l2, u3, t2) {
   throw n2;
 } }, "function" == typeof Promise ? Promise.prototype.then.bind(Promise.resolve()) : setTimeout, Math.random().toString(8);
 
-// node_modules/preact/jsx-runtime/dist/jsxRuntime.mjs
+// ../../node_modules/.pnpm/preact@10.29.7_preact-render-to-string@6.7.0/node_modules/preact/jsx-runtime/dist/jsxRuntime.mjs
 var f2 = 0;
 function u2(e2, t2, n2, o2, i2, u3) {
   t2 || (t2 = {});
